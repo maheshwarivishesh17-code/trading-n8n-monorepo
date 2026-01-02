7d7fd655e9b4fce347b344c51a095c337f2269ae
maheshwarivishesh17-code <maheshwarivishesh17@gmail,com>
2026-01-02 16:25:57 +0530

    added for deployment

--- apps/backend/middleware.ts
+++ apps/backend/middleware.ts
@@ -2,9 +2,7 @@ import type { NextFunction } from "express";
 const JWT_SECRET = process.env.JWT_SECRET!;
 import jwt, { type Jwt, type JwtPayload } from "jsonwebtoken";
 import type { Request, Response } from "express";
-export const config = {
-  runtime: 'nodejs',
-}
+export const runtime = 'nodejs'; // 'edge' is the default for middleware
 
 export function authMiddleware(req: Request, res: Response, next: NextFunction) {
   const header = req.headers["authorization"] as string;
